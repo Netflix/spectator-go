ccaa372edd1ec42d9fb536d49a6a060d015ae6bf
Daniel Muino <dmuino@netflix.com>
2019-06-05 11:27:07 -0700

    Remove unused functions from fd_noop
    `gometalinter` was complaining about them

--- fd_noop.go
+++ fd_noop.go
@@ -2,14 +2,6 @@
 
 package spectator
 
-func getNumFiles(dir string) (n int, err error) {
-	return 0, nil
-}
-
-func updateFdStats(s *sysStatsCollector, cur int, max uint64) {
-	// do nothing
-}
-
 func fdStats(s *sysStatsCollector) {
 	// do nothing
 }
